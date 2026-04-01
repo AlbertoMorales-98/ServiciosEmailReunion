04d844c98d83600c4e3c45f6ec30ecc47e3846c3
Albert <alberto.morales@dpr.icu>
2026-04-01 17:20:39 -0600

    Se agrega la parte de @CrossOrigin para pruebas

--- src/main/java/Correos/Contacto/Controller/ContactoController.java
+++ src/main/java/Correos/Contacto/Controller/ContactoController.java
@@ -4,12 +4,14 @@ import Correos.Contacto.Request.ContactoRequest;
 import Correos.Contacto.Service.EmailServiceContacto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/contacto")
 @RequiredArgsConstructor
 public class ContactoController {
--- src/main/java/Correos/Reunion/api/MeetingController.java
+++ src/main/java/Correos/Reunion/api/MeetingController.java
@@ -5,12 +5,14 @@ import lombok.RequiredArgsConstructor;
 import Correos.Reunion.api.dto.MeetingRequest;
 import Correos.Reunion.service.EmailServiceReunion;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/reuniones")
 @RequiredArgsConstructor
 public class MeetingController {
